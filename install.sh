906a398c7cfd1299b1e542df6adf78fdb683100f
Fernandos <worldwide@online.de>
2013-07-08 18:38:38 +0200

    added questions to allow adding the changes to conf files by the install script.

--- install.sh
+++ install.sh
@@ -5,6 +5,20 @@ read flag
 
 if [[ flag -eq "y" ]]; then
     cp -i "$service" "/usr/lib/systemd/system/"
-    systemctl enable $service
+    systemctl enable $service &&
     echo "$service successfully installed and enabled."
+fi
+
+echo "Should we load the radeon module during bootup? y/n"
+read bootup
+if [[ bootup -eq "y" ]]; then
+    echo radeon > /etc/modules-load.d/radeon.conf &&
+    echo "The 'radeon' module is now loaded during startup. See /etc/modules-load.d/radeon.conf"
+fi
+
+echo "Should we load the radeon module during bootup? y/n"
+read blacklist
+if [[ blacklist -eq "y" ]]; then
+    echo "blacklist radeon" >> /etc/modprobe.d/blacklist.conf &&
+    echo "Added the radeon module to the blacklist in /etc/modprobe.d/blacklist.conf"
 fi
